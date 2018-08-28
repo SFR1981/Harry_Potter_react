a535f8c9e0088ffa7f0e020d3680eb0a85254c51
Magdalena <m.mandat@gmail.com>
2018-08-28 13:32:39 +0100

    api is passing information through

---
+++ harry_potter/src/components/CharacterSelector.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CharacterSelector = (props) =>{
+
+
+ return <select>
+ <option disabled value="default"> Choose a character </option>
+</select>
+
+}
+
+
+
+
+
+
+
+export default CharacterSelector;
---
+++ harry_potter/src/containers/CharacterContainer.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import CharacterSelector from '../components/CharacterSelector';
+import CharacterDetail from '../components/CharacterDetail';
+
+
+class CharacterContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+      selectedCharacter: null
+    };
+    this.handleCharacterSelected = this.handleCharacterSelected.bind(this);
+  }
+
+  componentDidMount(){
+    const url = 'http://hp-api.herokuapp.com/api/characters';
+    fetch(url)
+    .then((res)=>{
+      return res.json();
+    })
+    .then((characters)=>{
+      this.setState({ characters: characters});
+    })
+  }
+
+
+handleCharacterSelected(index){
+  const selectedCharacter = this.state.characters[index];
+  this.setState({ selectedCharacter: selectedCharacter})
+
+}
+
+
+
+
+
+
+render() {
+  return (
+    <div>
+      <h1>Character Container</h1>
+      <CharacterSelector characters={this.state.characters} onCharacterSelected={this.handleCharacterSelected}/>
+      <CharacterDetail selectedCharacter={this.state.selectedCharacter}/>
+    </div>
+  );
+}
+}
+
+
+
+
+
+
+export default CharacterContainer;
